777fc35807fefeb7aad9777b5757448fca015135
VChet <17050347+VChet@users.noreply.github.com>
2020-07-15 21:33:50 +0500

    fix(style): handle github api errors

--- api/styles.js
+++ api/styles.js
@@ -21,27 +21,34 @@ async function retrieveRepositoryData(link) {
   const repoURL = new URL(link);
   const pathname = repoURL.pathname;
 
-  const [contents, repo] = await Promise.all([
-    axios.get(`https://api.github.com/repos${pathname}/contents`),
-    axios.get(`https://api.github.com/repos${pathname}`)
-  ]);
-  const usercss = contents.data.find(file => file.name.includes("user.css"));
+  try {
+    const [contents, repo] = await Promise.all([
+      axios.get(`https://api.github.com/repos${pathname}/contents`),
+      axios.get(`https://api.github.com/repos${pathname}`)
+    ]);
+    const usercss = contents.data.find(file => file.name.includes("user.css"));
 
-  return {
-    url: link,
-    usercss: usercss.download_url,
-    name: repo.data.name.replace(/-/g, " "),
-    description: repo.data.description,
-    owner: repo.data.owner.login,
-    lastUpdate: repo.data.updated_at,
-    stargazers: repo.data.stargazers_count,
-    watchers: repo.data.subscribers_count,
-    forks: repo.data.forks,
-    issues: repo.data.open_issues,
-    license: repo.data.license,
-    isArchived: repo.data.archived,
-    isFork: repo.data.fork
-  };
+    return {
+      url: link,
+      usercss: usercss.download_url,
+      name: repo.data.name.replace(/-/g, " "),
+      description: repo.data.description,
+      owner: repo.data.owner.login,
+      lastUpdate: repo.data.updated_at,
+      stargazers: repo.data.stargazers_count,
+      watchers: repo.data.subscribers_count,
+      forks: repo.data.forks,
+      issues: repo.data.open_issues,
+      license: repo.data.license,
+      isArchived: repo.data.archived,
+      isFork: repo.data.fork
+    };
+  } catch (error) {
+    return {
+      status: error.response.status,
+      error: error.response.statusText
+    };
+  }
 }
 
 function getStyleData(req, res) {
@@ -49,6 +56,8 @@ function getStyleData(req, res) {
     if (error) return res.status(500).json({ error });
 
     const data = await retrieveRepositoryData(style.repoLink);
+    if (data.error) return res.status(data.status).json({ error: data.error });
+
     res.status(200).json({ data });
   });
 }
