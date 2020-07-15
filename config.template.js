06255d7c921872c25c766a3ce31625255b35c4ff
VChet <17050347+VChet@users.noreply.github.com>
2020-07-15 23:40:48 +0500

    chore(config): remove unused fields

--- config.template.js
+++ config.template.js
@@ -1,12 +1,4 @@
 module.exports = {
   appPort: "7540",
-  mongoUrl: "mongodb://localhost/stylebase",
-  appSalt: "SALT",
-  session: {
-    secret: "SECRET",
-    name: "stylebase.sid",
-    proxy: true,
-    resave: true,
-    saveUninitialized: false
-  }
+  mongoUrl: "mongodb://localhost/stylebase"
 };
