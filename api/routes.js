60cd1158728a626dc4193d3cf31e4fe61290398e
VChet <17050347+VChet@users.noreply.github.com>
2020-09-04 20:48:30 +0500

    feat(server): add rate-limiter

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const apicache = require("apicache");
+const rateLimit = require("express-rate-limit");
 
 const router = express.Router();
 const cache = apicache.middleware;
 
 const onlyStatus200 = (req, res) => res.statusCode === 200;
 const cacheSuccessful = cache("10 minutes", onlyStatus200);
+
+// Allow 1 request per 10 minutes
+const GHRateLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 1,
+  message:
+    "Too many update requests made from this IP, please try again after 10 minutes"
+});
 
 const {
   getStyles,
@@ -23,8 +32,8 @@ router.get("/style/:id", cacheSuccessful, getStyleData);
 router.get("/search/:page?", searchStyle);
 router.get("/owner/:owner/:page?", cacheSuccessful, getStylesByOwner);
 router.post("/style/add", addStyle);
-router.put("/style/update/all", updateAllStyles);
-router.put("/style/update/:id", updateStyle);
+router.put("/style/update/all", GHRateLimiter, updateAllStyles);
+router.put("/style/update/:id", GHRateLimiter, updateStyle);
 router.delete("/style/delete", deleteStyle);
 
 module.exports = router;
