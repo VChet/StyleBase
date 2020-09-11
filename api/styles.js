3bc5fb16f0c9cf9455a50f60995420148ea4147e
VChet <17050347+VChet@users.noreply.github.com>
2020-09-11 19:35:33 +0500

    fix(style): remove 'added' field, add 'created' field
    'added' field was filling wrong, removed for now.
    'created' field is parsed from repository, equals to repository creation date

--- api/styles.js
+++ api/styles.js
@@ -36,6 +36,7 @@ async function retrieveRepositoryData(link) {
       name: repo.data.name.replace(/-/g, " "),
       description: repo.data.description,
       owner: repo.data.owner.login,
+      created: repo.data.created_at,
       lastUpdate: repo.data.updated_at,
       stargazers: repo.data.stargazers_count,
       watchers: repo.data.subscribers_count,
