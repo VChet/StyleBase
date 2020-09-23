5aaa0f1901d98c63ef223c4e826617d47670add2
VChet <17050347+VChet@users.noreply.github.com>
2020-09-23 23:50:08 +0500

    chore(authentication): add passport and session libs, add configurations

--- config.template.js
+++ config.template.js
@@ -1,6 +1,17 @@
 module.exports = {
   appPort: "7540",
   mongoUrl: "mongodb://localhost/stylebase",
+  session: {
+    secret: "SECRET",
+    name: "stylebase.sid",
+    proxy: true,
+    resave: true,
+    saveUninitialized: false
+  },
+  GHOAuth: {
+    clientId: "",
+    clientSecret: ""
+  },
   GHToken: "Personal access token",
   CaptchaSecretKey: ""
 };
