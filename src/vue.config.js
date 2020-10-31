61566f1cbbcc3e891058ca3dac2bf296910caa1d
VChet <17050347+VChet@users.noreply.github.com>
2020-10-31 20:11:21 +0500

    fix(vue-loader): preserve whitespace

--- src/vue.config.js
+++ src/vue.config.js
@@ -10,6 +10,15 @@ module.exports = {
     }
   },
 
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((args) => {
+        args.compilerOptions.whitespace = 'preserve';
+      });
+  },
+
   devServer: {
     proxy: {
       '^/api': {
