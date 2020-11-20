2b7302c238b994db0afb7f43ee98d1ecc8b345d6
VChet <17050347+VChet@users.noreply.github.com>
2020-11-20 22:11:33 +0500

    fix(footer): increase links font size and paddings in footer on mobile

--- src/src/components/AppFooter.vue
+++ src/src/components/AppFooter.vue
@@ -1,9 +1,17 @@
 <template>
   <footer>
     <div class="container">
+      <ul class="link-list">
+        <li>
       <a href="mailto:feedback@stylebase.cc" rel="noopener">Contact Us</a>
+        </li>
+        <li>
       <button class="link" type="button" @click="$emit('open-nav-link', 'showPrivacyModal')">Privacy Policy</button>
+        </li>
+        <li>
       <a href="https://github.com/VChet/StyleBase" rel="noopener" target="_blank">Code on GitHub</a>
+        </li>
+      </ul>
     </div>
   </footer>
 </template>
@@ -22,12 +30,22 @@ footer {
   .container {
     padding-top: 0.25rem;
     padding-bottom: 0.25rem;
-    text-align: right;
   }
 
-  button,
-  a:not(:last-child) {
-    margin-right: 1rem;
+  .link-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+    gap: 0.75rem 1rem;
+    margin: 0;
+    list-style-type: none;
+
+    @include media-size-mobile {
+      flex-flow: column nowrap;
+      padding-top: 0.5rem;
+      padding-bottom: 0.5rem;
+      font-size: 16px;
+    }
   }
 }
 </style>
