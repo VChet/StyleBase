ae03ddc0946d3235c732babc6d8ff633dc3df502
VChet <17050347+VChet@users.noreply.github.com>
2020-11-22 00:11:41 +0500

    feat(confirmation): add confirmation dialog component

---
+++ src/src/components/dialogs/ConfirmationDialog.vue
@@ -0,0 +1,44 @@
+<template>
+  <base-dialog v-if="open" size="small" @close="$emit('close')">
+    <div class="dialog-title">{{ message }}</div>
+    <div class="dialog-buttons">
+      <button class="style-button" type="button" @click="$emit('close')">Cancel</button>
+      <button class="style-button" type="button" @click="$emit('confirm')">Proceed</button>
+    </div>
+  </base-dialog>
+</template>
+
+<script>
+import BaseDialog from '@/components/dialogs/BaseDialog';
+
+export default {
+  name: 'ConfirmationDialog',
+  components: {
+    BaseDialog
+  },
+  props: {
+    open: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    message: {
+      type: String,
+      default: 'Perform an action?'
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.dialog-title {
+  font-size: 30px;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+.dialog-buttons {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
