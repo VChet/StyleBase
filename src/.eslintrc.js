63aaf560ecbd37cb06cb9e0a233e75eb0df0a22c
VChet <17050347+VChet@users.noreply.github.com>
2020-11-22 00:24:32 +0500

    chore(eslint): add component-name-in-template-casing rule

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/component-name-in-template-casing': ['warn', 'PascalCase']
   }
 };
