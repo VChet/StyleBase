8f6e6a94f68aeca262d7a107aaf3a8ddca6930c0
VChet <17050347+VChet@users.noreply.github.com>
2020-12-16 00:05:55 +0500

    chore(cache): replace apicache lib with memory-cache

--- api/routes-api.js
+++ api/routes-api.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const apicache = require("apicache");
+const mcache = require("memory-cache");
 const rateLimit = require("express-rate-limit");
 
 const { Style } = require("../models/Style");
@@ -7,13 +7,25 @@ const { Style } = require("../models/Style");
 const router = express.Router();
 
 // Cache
-const cache = apicache.middleware;
-const onlyStatus200 = (req, res) => process.env.NODE_ENV === "production" && res.statusCode === 200;
-const cacheSuccessful = cache("10 minutes", onlyStatus200);
-const clearCache = (req, res, next) => {
-  apicache.clear();
+const cache = (duration) => (req, res, next) => {
+  const key = `__express__${req.originalUrl}` || req.url;
+  const cachedBody = mcache.get(key);
+  if (cachedBody) return res.send(cachedBody);
+
+  res.sendResponse = res.send;
+  res.send = (body) => {
+    if (!process.env.NODE_ENV === "production" && res.statusCode === 200) {
+      mcache.put(key, body, duration * 60 * 1000);
+    }
+    res.sendResponse(body);
+  };
   next();
 };
+const clearCache = (req, res, next) => {
+  mcache.clear();
+  next();
+};
+
 // Rate limiter (allow 30 request per 60 minutes)
 const rateLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
@@ -59,12 +71,12 @@ const {
   getCurrentUser
 } = require("./users");
 
-router.get("/styles/:owner?", cacheSuccessful, getStyles);
-router.get("/style/files", getRepositoryFiles);
-router.get("/style", cacheSuccessful, getStyleData);
+router.get("/styles/:owner?", cache(10), getStyles);
+router.get("/style/files", cache(10), getRepositoryFiles);
+router.get("/style", cache(10), getStyleData);
 router.post("/style/add", rateLimiter, addStyle);
-router.put("/style/update/all", rateLimiter, updateAllStyles);
-router.put("/style/update", rateLimiter, updateStyle);
+router.put("/style/update/all", rateLimiter, clearCache, updateAllStyles);
+router.put("/style/update", rateLimiter, clearCache, updateStyle);
 router.patch("/style/edit", isAuthorized, clearCache, editStyle);
 router.delete("/style/delete", isAuthorized, clearCache, deleteStyle);
 
