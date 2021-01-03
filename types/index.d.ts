525b3bed1da67384f547691f768a01ecaba4411b
VChet <17050347+VChet@users.noreply.github.com>
2021-01-03 22:36:29 +0500

    refactor(types): move module declarations to index

--- types/index.d.ts
+++ types/index.d.ts
@@ -62,3 +62,55 @@ declare module "passport-gitea" {
     authenticate(req: express.Request, options?: passport.AuthenticateOptions): void;
   }
 }
+
+declare module "repo-images" {
+  export interface repoOptions {
+    token?: string;
+    access_token?: string;
+    branch?: string;
+  }
+
+  export interface Image {
+    path: string;
+    mode: string;
+    type: string;
+    sha: string;
+    size: number;
+    url: string;
+    rawgit: string;
+  }
+
+  export default function (repo: string, opts: repoOptions, callback?: Function): Promise<Array<Image>>;
+}
+
+declare module "usercss-meta" {
+  interface ParseError {
+    name: string;
+    code: string;
+    message: string;
+    index: number;
+  }
+
+  interface ParseResults {
+    metadata: {
+      name: string;
+      version: string;
+      namespace: string;
+      author: string;
+      description: string;
+      homepageURL: string;
+      supportURL: string;
+      updateURL: string;
+      license: string;
+      preprocessor: string;
+    };
+    errors: Array<ParseError>;
+  }
+
+  export interface MetaParser {
+    parse(text: string, options?: any): ParseResults;
+  }
+
+  const metaParser: MetaParser;
+  export default metaParser;
+}
