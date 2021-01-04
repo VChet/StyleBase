5c6635438881624b625cda245275c3e8e7dc7b2c
VChet <17050347+VChet@users.noreply.github.com>
2021-01-04 20:05:23 +0500

    docs(metadata): add JSDoc for metadata fields
    https://github.com/openstyles/stylus/wiki/Writing-UserCSS#usercss-metadata

--- types/index.d.ts
+++ types/index.d.ts
@@ -28,16 +28,48 @@ declare module "usercss-meta" {
 
   interface ParseResults {
     metadata: {
+      /**
+       * The name of UserCSS style.
+       * The combination of @name and @namespace must be unique for each UserCSS.
+       */
       name: string;
-      version: string;
+      /**
+       * The namespace of the UserCSS.
+       * Helps to distinguish between styles with the same name.
+       * Usually, the author's nickname or homepage. Can contain spaces and special characters.
+       */
       namespace: string;
-      author: string;
-      description: string;
-      homepageURL: string;
-      supportURL: string;
-      updateURL: string;
-      license: string;
-      preprocessor: string;
+      /**
+       * The version of the style.
+       * This is used for the update check.
+       * Follow {@link http://semver.org/|semver} versioning.
+       */
+      version: string;
+      /** A short significant description. */
+      description?: string;
+      /** Info about the author of the UserCSS style. */
+      author?: string;
+      /** The project's homepage that is used in Stylus' Manage and Edit pages to link to UserCSS source. */
+      homepageURL?: string;
+      /** The URL the user can report issues to the UserCSS author. */
+      supportURL?: string;
+      /**
+       * When defined, this URL is used when updating the style
+       * otherwise the style is updated from where it was installed.
+       */
+      updateURL?: string;
+      /**
+       * Include a license based on the
+       * {@link https://spdx.org/licenses|SPDX license identifier}.
+       */
+      license?: string;
+      /** Applies a CSS preprocessor. */
+      preprocessor?: string;
+      /**
+       * Defines a live-switchable variable which will be compiled to valid CSS
+       * according to the preprocessor that is set.
+       */
+      var?: string;
     };
     errors: Array<ParseError>;
   }
